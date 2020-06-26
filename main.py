e0a6ae3cd53a01bcc6ecfbed7f8c4967cb171c49
pasinduwalawaka <pasindualawaka@gmail.com>
2020-06-26 06:39:14 +0000

    archi diagram

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from yaml import load, dump
 
+import argparse
+
+
+
+
 def main():
 # display some lines
+    parser = argparse.ArgumentParser(description='Generate a HyperLedger Network.')
+    parser.add_argument('no_of_orderer_nodes', type=int, help='')
+    parser.add_argument('no_of_peer_nodes', type=int, help='')
+
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
     print(1)
 
 if __name__ == "__main__": main()
